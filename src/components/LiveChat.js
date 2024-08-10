490bfe967ff625fbbd5aa53edc86aa5ef06bb948
Mohit sharma <mohitanand8987@gmail.com>
2024-08-11 02:06:47 +0530

    added chat feature with dummy data

---
+++ src/components/LiveChat.js
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import ChatMessage from "./ChatMessage";
+import { useDispatch, useSelector } from "react-redux";
+import { addMessage } from "../utils/chatSlice";
+import { generateRandomName, makeRandomMessage } from "../utils/helper";
+
+const LiveChat = () => {
+  const dispatch = useDispatch();
+  const chatMessages = useSelector((store) => store.chat.messages);
+  useEffect(() => {
+    const i = setInterval(() => {
+      // API Polling
+      console.log("Test");
+      dispatch(
+        addMessage({
+          name: generateRandomName(),
+          message: makeRandomMessage(30),
+        })
+      );
+    }, 2000);
+    return () => clearInterval(i);
+  }, []);
+  return (
+    <div className="w-full h-[500px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse">
+      {chatMessages.map((c, i) => (
+        <ChatMessage key={i} name={c.name} message={c.message} />
+      ))}
+    </div>
+  );
+};
+
+export default LiveChat;
--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/appSlice";
 import { useSearchParams } from "react-router-dom";
 import CommentContainer from "./CommentContainer";
+import LiveChat from "./LiveChat";
 
 const WatchPage = () => {
   const dispatch = useDispatch();
@@ -11,16 +12,21 @@ const WatchPage = () => {
     dispatch(closeMenu());
   }, []);
   return (
-    <div className="flex flex-col">
-      <div className="px-5">
-        <iframe
-          width="750"
-          height="400"
-          src={"https://www.youtube.com/embed/" + searchParams.get("v")}
-          title="fg"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullScreen
-        ></iframe>
+    <div className="flex flex-col w-full">
+      <div className="px-5 flex w-full">
+        <div>
+          <iframe
+            width="1000"
+            height="500"
+            src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+            title="fg"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <div className="w-full">
+          <LiveChat />
+        </div>
       </div>
       <CommentContainer />
     </div>
