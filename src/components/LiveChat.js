bd7be8210ef465a98ae7030a198d2b9512cbe97d
Mohit sharma <mohitanand8987@gmail.com>
2024-08-11 13:00:44 +0530

    created live chat

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,31 +1,57 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ChatMessage from "./ChatMessage";
 import { useDispatch, useSelector } from "react-redux";
 import { addMessage } from "../utils/chatSlice";
 import { generateRandomName, makeRandomMessage } from "../utils/helper";
 
 const LiveChat = () => {
+  const [liveMessage, setLiveMessage] = useState("");
   const dispatch = useDispatch();
   const chatMessages = useSelector((store) => store.chat.messages);
   useEffect(() => {
     const i = setInterval(() => {
       // API Polling
-      console.log("Test");
       dispatch(
         addMessage({
           name: generateRandomName(),
           message: makeRandomMessage(30),
         })
       );
-    }, 2000);
+    }, 1500);
     return () => clearInterval(i);
   }, []);
   return (
-    <div className="w-full h-[500px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse">
-      {chatMessages.map((c, i) => (
-        <ChatMessage key={i} name={c.name} message={c.message} />
-      ))}
-    </div>
+    <>
+      <div className="w-full h-[500px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse">
+        <div>
+          {chatMessages.map((c, i) => (
+            <ChatMessage key={i} name={c.name} message={c.message} />
+          ))}
+        </div>
+      </div>
+      <form
+        className="w-full p-2 ml-2 border border-black"
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log("Submitted");
+          dispatch(
+            addMessage({
+              name: "Mohit",
+              message: liveMessage,
+            })
+          );
+          setLiveMessage("");
+        }}
+      >
+        <input
+          className="px-2  w-80"
+          type="text"
+          value={liveMessage}
+          onChange={(e) => setLiveMessage(e.target.value)}
+        />
+        <button className="px-2 mx-2 bg-green-100">Send</button>
+      </form>
+    </>
   );
 };
 
--- src/utils/const.js
+++ src/utils/const.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "";
+export const LIVE_CHAT_COUNT = 25;
 export const YOUTUBE_VIDEO_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&maxResults=50&chart=mostPopular&regionCode=IN&key=" +
   process.env.REACT_APP_GOOGLE_API_KEY;
